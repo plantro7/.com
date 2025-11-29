a9b503b33549855bcf10fa70ca97db7ec87b6db5
Soumit <sausoumit@gmail.com>
2025-11-29 23:19:49 +0530

    Add version logging for debugging

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+console.log("App Version: 2025-11-29 23:20 (Groq Fix)");
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
