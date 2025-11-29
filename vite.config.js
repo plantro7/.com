d4871b8df59fa250b8dac4683b843a9e63a1ad31
Soumit <sausoumit@gmail.com>
2025-11-29 22:43:52 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/.com/",
   plugins: [react()],
   build: {
-    outDir: 'build_artifact'
+    outDir: 'dist'
   }
 })
